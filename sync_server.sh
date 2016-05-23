07fb04c647dc8f9988065b70d501962482a84a0d
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-05-23 21:31:46 +0530

    Mild modifications

--- sync_server.sh
+++ sync_server.sh
@@ -2,5 +2,5 @@
 # Sync the current folder to localhost
 # Every 30 seconds
 # Avoid updating localhost html folder everytime
-
-while [ 1 ]; do sh launch.sh; date;  sleep 30; done
+count=1
+while [ 1 ]; do sh launch.sh; echo -n $count' ' ; date; count=$((count+1)); sleep 30; done
