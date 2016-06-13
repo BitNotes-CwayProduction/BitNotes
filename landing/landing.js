379ec6b8981aa1087e3db06a12721fe11cbc92c6
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-06-13 17:15:30 +0530

    Landing page almost ready

--- landing/landing.js
+++ landing/landing.js
@@ -1,6 +1,7 @@
 function addListeners() {
 	listenToSearchIcon();
 	showQuickStats();
+	likeHit();
 }
 
 function listenToSearchIcon() {
@@ -16,12 +17,42 @@ function showQuickStats() {
 		var cond = document.getElementById('quickstats-menu');
 		// console.log('sob clicked');
 		if (parseInt(cond.style.marginLeft) < 0) {
+			this.getElementsByTagName('span')[0].style.color = "blue";
 			cond.style.marginLeft = 10;
 		} else {
+			this.getElementsByTagName('span')[0].style.color = "black";
 			cond.style.marginLeft = -1000;
 		}
 		// console.log('function complete');
 	});
 }
 
+function likeHit() {
+	var buttonList = document.getElementsByClassName('content-likes');
+	for (var i = 0; i < buttonList.length; i++) {
+		buttonList[i].style.opacity = '0.2';
+		 buttonList[i].parentElement.addEventListener('click', function() {
+			 var curbutton = this.getElementsByClassName('content-likes')[0];
+			//  curbutton.classList.add('content-likes-animate');
+			 if (curbutton.style.animation == '') {
+				 curbutton.style.animation = 'pound .5s';
+			 } else {
+				 curbutton.style.animation = '';
+			 }
+			 var likes = parseInt(this.getElementsByClassName('content-likes-number')[0].innerHTML);
+			 if (curbutton.style.opacity == '0.2') {
+				 curbutton.style.opacity = '1';
+				 this.getElementsByClassName('content-likes-number')[0].innerHTML = (likes+1);
+			 } else {
+				 curbutton.style.opacity = '.2';
+				 this.getElementsByClassName('content-likes-number')[0].innerHTML = (likes-1);
+			 }
+			 curbutton.blur();
+		 });
+	}
+}
 addListeners();
+
+document.getElementsByClassName('content-stats')[0].addEventListener('click', function() {
+	// console.log('sob clicked');
+});
